c589ce19743fb668d33dc4661809953c2950a3bc
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-09-26 02:36:46 +0600

    heading added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>CAR CITY</h1>
+            <p>
+                If all the cars in the United States were placed end to end, it
+                would probably be Labor Day Weekend.
+            </p>
+            <h2>All Car Price:- $500 Million Dollar</h2>
+        </div>
+    );
+};
+
+export default Header;
